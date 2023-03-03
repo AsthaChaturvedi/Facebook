4f670621a4f5cee3aca4e8c73abdde4d17046983
Apple Care <applecare@Apples-MacBook-Pro.local>
2023-03-03 11:34:25 +0530

    facebook

---
+++ index.js
@@ -0,0 +1,5 @@
+const Submit=()=>{
+ let emailid =   document.getElementById("emailid").value;
+ let password =   document.getElementById("password").value;
+ alert(JSON.stringify({emailid,password}))
+}
